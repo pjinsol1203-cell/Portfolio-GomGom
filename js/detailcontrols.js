064467f19645b01637b18ba1356f8b71b3f05b78
jinsol <json_1203@naver.com>
2025-11-06 16:08:38 +0900

    add detail page

---
+++ js/detailcontrols.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+    let productId = getQueryParam("cid")
+    const result = product.find(item=>item.pid == productId)
+    let thumbSmallListContents =""
+    result.pthumbArray.forEach((thumb,index)=>{
+        thumbSmallListContents+=`<li><img src="./img/${thumb}" alt="${result.pnameKo} ${index+1}번 사진"></li>`
+    })
+
+    const detail1 = document.querySelector(".detail-1")
+    detail1.innerHTML =`<section class="detail-thumb">
+                <figure class="thumb-big">
+                    <img src="./img/${result.pthumb}" alt="${result.pnameEn} 사진">
+                </figure>
+
+                <div class="thumb-slider slider-container">
+                    <ul class="thumb-small-list">
+                         ${thumbSmallListContents}
+                    </ul>
+                </div>
+            </section>
+            <section class="detail-desc">
+              <h3>${result.pnameKo}</h3>
+                <h4><em>${result.pnameEn}</em></h4>
+                <p class="desc">${result.pdesc}</p>
+                <p class="sale"><span class="per-sale">${result.salePer}%</span>
+                <span class="price-origin">${result.pprice.toLocaleString("ko-KR")}원</span></p>
+                <p class="price">${(Math.round(result.pprice*(1-(result.salePer*0.01)))).toLocaleString("ko-KR")}원</p>
+                <div class="buy">
+                    <a href="#">장바구니</a>
+                    <a href="#" class="btn-buy">구매하기</a>
+                </div>
+            </section>
+            `
+})
--- js/product-control.js
+++ js/product-control.js
@@ -13,10 +13,25 @@ document.addEventListener("DOMContentLoaded",()=>{
                 <h3>${item.pnameKo}</h3>
                 <h4><em>${item.pnameEn}</em></h4>
                 <p class="ln3">${item.pdesc}</p>
-                <p class="price">${item.pprice}원</p>
+                <p class="price">${item.pprice.toLocaleString('ko-KR')}원</p>
             </a>
         </li>`
     })
     list.innerHTML = res
 
+   // 하트 아이콘 클릭 이벤트
+    list.querySelectorAll(".heart").forEach(heart=>{
+        heart.addEventListener("click",(e)=>{
+            e.preventDefault()
+            e.stopPropagation()
+            const heartImg = heart.querySelector("img")
+            if(heartImg.src.includes("heart-empty")){
+                heartImg.src = "./img/heart-full.svg"
+                heartImg.alt = "좋아요 선택함"
+            }else{
+                heartImg.src = "./img/heart-empty.svg"
+                heartImg.alt = "좋아요 선택안함"
+            }
+        })
+    })
 })
